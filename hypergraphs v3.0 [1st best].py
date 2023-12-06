9713be22cc8047813412c6468f8f35dca9586eb1
Karol Błaszczak <92331517+karcio424@users.noreply.github.com>
2023-12-06 23:38:20 +0100

    Minor changes

---
+++ hypergraphs v3.0 [1st best].py
@@ -0,0 +1,118 @@
+import matplotlib.pyplot as plt
+import networkx as nx
+import itertools
+import plotly.graph_objs as go
+
+
+def read_dimacs_cnf(filename):
+    clauses = []
+    with open(filename, 'r') as file:
+        for line in file:
+            if line.startswith("c"):
+                continue
+            if line.startswith("p cnf"):
+                num_vars, num_clauses = map(int, line.strip().split()[2:])
+            else:
+                clause = list(map(int, line.strip().split()[:-1]))
+                clauses.append(clause)
+    return num_vars, clauses
+
+def draw_hypergraph(num_vars, clauses):
+    G = nx.Graph()
+
+    for var in range(1, num_vars + 1):
+        G.add_node(var)
+
+    for i, clause in enumerate(clauses):
+        G.add_nodes_from(clause)
+        for pair in itertools.combinations(clause, 2):
+            G.add_edge(*pair, clause=i+1)
+
+    node_colors = []
+    for node in G.nodes():
+        color = 0
+        for i, clause in enumerate(clauses):
+            if node in clause:
+                color = i + 1
+                break
+        node_colors.append(color)
+
+    pos = nx.spring_layout(G)
+    
+    nx.draw(G, pos, with_labels=True, font_weight='bold', node_color=node_colors, cmap=plt.cm.Blues, node_size=800)
+
+def interactive_hypergraph(num_vars, clauses):
+    G = nx.Graph()
+
+    for var in range(1, num_vars + 1):
+        G.add_node(var)
+    for i, clause in enumerate(clauses):
+        G.add_nodes_from(clause)
+        for pair in itertools.combinations(clause, 2):
+            G.add_edge(*pair, clause=i+1)
+
+    pos = nx.spring_layout(G)
+
+    edge_trace = go.Scatter(
+        x=[],
+        y=[],
+        line=dict(width=0.5, color='#888'),
+        hoverinfo='none',
+        mode='lines')
+
+    for edge in G.edges():
+        x0, y0 = pos[edge[0]]
+        x1, y1 = pos[edge[1]]
+        edge_trace['x'] += (x0, x1, None)
+        edge_trace['y'] += (y0, y1, None)
+
+    node_trace = go.Scatter(
+        x=[],
+        y=[],
+        text=[],
+        mode='markers+text',
+        hoverinfo='text',
+        marker=dict(
+            showscale=True,
+            colorscale='Blues',
+            reversescale=True,
+            color=[],
+            size=10,
+            colorbar=dict(
+                thickness=15,
+                title='Numer klauzuli',
+                xanchor='left',
+                titleside='right'
+            ),
+            line=dict(width=2)))
+
+    for node in G.nodes():
+        x, y = pos[node]
+        node_trace['x'] += (x,)
+        node_trace['y'] += (y,)
+        node_trace['text'] += ('Var: ' + str(node),)
+
+    for node, adjacencies in enumerate(G.adjacency()):
+        node_trace['marker']['color'] += (len(adjacencies[1]),)
+
+    layout = go.Layout(
+        title='Interaktywna wizualizacja hipergrafu',
+        titlefont=dict(size=16),
+        showlegend=False,
+        hovermode='closest',
+        margin=dict(b=20, l=5, r=5, t=40),
+        annotations=[dict(
+            text="",
+            showarrow=False,
+            xref="paper", yref="paper")],
+        xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+        yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
+
+    fig = go.Figure(data=[edge_trace, node_trace], layout=layout)
+    fig.show()
+
+
+filename = "DIMACS_files/turbo_easy/example_2.cnf"
+num_vars, clauses = read_dimacs_cnf(filename)
+draw_hypergraph(num_vars, clauses)
+interactive_hypergraph(num_vars, clauses)
